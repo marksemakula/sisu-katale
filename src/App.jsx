d552419ae1a8a1d8b867c82cd026881a8f0582b0
Mark SEMAKULA™ <marksemakula@gmail.com>
2026-05-06 19:32:14 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ScoobyKatale from './pages/ScoobyKatale';
+import ScoobyGulu from './pages/ScoobyGulu';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <ScoobyKatale />
+      <ScoobyGulu />
     </BrowserRouter>
   );
 }
